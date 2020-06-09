f7e8a89ba75ffd29ecfb1af593a3a1f577065482
tkuratty <tkuratty@gmail.com>
2020-06-09 11:47:42 +0900

    build(package.json): Remove eslint (use original)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import CityViewer from "./CityViewer/CityViewer";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-        <Button variant="primary">Primary</Button>
+        <CityViewer />
       </header>
     </div>
   );
---
+++ src/CityViewer/CityViewer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const CityViewer: React.FC = () => {
+  return (
+    <div>
+      <p>City information</p>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+};
+
+export default CityViewer;
