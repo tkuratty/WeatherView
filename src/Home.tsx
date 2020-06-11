d2cafe4e25f6175be639d0a4f76e9e15b9f3a877
tkuratty <tkuratty@gmail.com>
2020-06-11 13:44:12 +0900

    feat(Home): Add basic layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Pref } from "./Utils/Area";
+import { Row, Col } from "react-bootstrap";
 
 const Home: React.FC<{ area: Array<Pref> }> = (props) => {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <Row>
+      <Col sm={4}>City Selector</Col>
+      <Col>Weather Information</Col>
+    </Row>
   );
 };
 
